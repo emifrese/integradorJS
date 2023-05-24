d3cab0b828e1ef84a75c61c6be87cbe6a0003508
Emiliano Frese <emilianomartindominguezrese@gmail.com>
2023-05-24 10:07:50 -0300

    started card design

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const testButton = document.querySelector("#infoMl");
+// const testButton = document.querySelector("#infoMl");
 
-const getInfo = async () => {
-  const data = await fetch("https://api.mercadolibre.com/sites/MLA");
+// const getInfo = async () => {
+//   const data = await fetch("https://api.mercadolibre.com/sites/MLA");
 
-  const response = await data.json();
-  console.log(response);
-};
+//   const response = await data.json();
+//   console.log(response);
+// };
 
-testButton.addEventListener("click", getInfo);
+// testButton.addEventListener("click", getInfo);
