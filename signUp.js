8458bbcc1b1a479a3854b75224c865c3aefe9270
emifrese <emilianomartindominguezfrese@gmail.com>
2023-06-08 20:56:36 -0300

    finished logic for registering users

--- signUp.js
+++ signUp.js
@@ -6,28 +6,42 @@ const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const repeatPasswordInput = document.querySelector("#repeatPassword");
 const termsAndConditions = document.querySelector("#terms");
-const existingUsers = localStorage.getItem("users")
+const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+console.log(existingUsers)
 
 const isEmpty = (inputsArray) => {
-  return inputsArray.filter((input) => input.value === "" || !input.value);
+  return inputsArray.map((input) =>
+    input.value === "" || !input.value
+      ? { ...input, isError: true }
+      : { ...input, isError: false }
+  );
 };
 
 const manageErrors = (array, msg) => {
   for (let input of array) {
     const errorElement = input.input.parentElement.children[1];
-    if (input.input.id !== "terms") {
-      errorElement.innerText = msg;
-      errorElement.classList.remove("displayNone");
-      errorElement.classList.add("inputError");
+    if(input.isError){
+      if (input.input.id !== "terms") {
+        errorElement.innerText = msg;
+        errorElement.classList.remove("displayNone");
+        errorElement.classList.add("inputError");
+      } else {
+        showModal("Acepta los terminos y condiciones");
+      }
     } else {
-      showModal("Acepta los terminos y condiciones");
-      console.log("show modal");
+      if(input.input.id !== "terms"){
+        errorElement.innerText = "";
+        errorElement.classList.add("displayNone");
+        errorElement.classList.remove("inputError");
+      }
     }
   }
 };
 
 const submitRegisterHandler = (e) => {
   e.preventDefault();
+  let validForm = true;
 
   const inputValues = [
     { value: nameInput.value.trim(), input: nameInput },
@@ -37,31 +51,64 @@ const submitRegisterHandler = (e) => {
     { value: repeatPasswordInput.value.trim(), input: repeatPasswordInput },
     { value: termsAndConditions.checked, input: termsAndConditions },
   ];
-  // validacion general
+
+  const password = inputValues.filter((input) => input.input.id === "password");
+  const repeatPassword = inputValues.filter((input) => input.input.id === "repeatPassword")
+  const email = inputValues.filter((input) => input.input.id === "email");
+  // validacion genera
   
-  // email no valido
-  const email = inputValues.filter(input => input.input.id === "email")
-  console.log(email, /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email.value))
-  if(email.value !== "" && !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email.value)) {
-    manageErrors(email, "Ingrese un mail válido")
-  }
-  
-//  inputs vacios
+  console.log(validForm)
+
+  //  inputs vacios
   const emptyInput = isEmpty(inputValues);
   if (emptyInput.length > 0) {
     manageErrors(emptyInput, "El campo no debe estar vacío");
-    return;
+    if(emptyInput.some(input => input.isEmpty)){
+      validForm = false
+    }
+  }
+  // email no valido
+  if (
+    email[0].value !== "" &&
+    !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email[0].value)
+  ) {
+    console.log('email error')
+    manageErrors([{...email[0], isError: true}], "Ingrese un mail válido");
+    validForm = false;
+  }
+  // contraseñas no coinciden
+  if (password[0].value !== repeatPassword[0].value) {
+    manageErrors(
+      [{...password[0], isError: true}, {...repeatPassword[0], isError: true}],
+      "Las contraseñas no coinciden"
+    );
+    validForm = false;
+  }
+  // usuario repetido
+  console.log(existingUsers.find(user => user.email === email[0].value))
+  if(existingUsers.length > 0 && existingUsers.find(user => user.email === email[0].value)){
+    manageErrors([{...email[0], isError: true}], "Ese email ya fue utilizado")
+    validForm = false;
   }
 
-
-  // contraseñas no coinciden
-
-
-  // usuario repetido
-
+  if(!validForm){
+    return
+  }
+  
   // guardar usuario
+  const newUser = {
+    email: email[0].value,
+    password: password[0].value,
+    shoppingCart: [],
+    likes: [],
+  }
+
+  const newUsersArray = [...existingUsers, newUser]
+
+  localStorage.setItem("users", JSON.stringify(newUsersArray))
 
   // logearlo
+  location.replace("./index.html")
 };
 
 (() => {
